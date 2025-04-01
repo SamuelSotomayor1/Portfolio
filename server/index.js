8806bc003a66e4b684ae61376c81280a6f2ea1d5
SamuelSotomayor1 <andressotomayor477@gmail.com>
2025-04-01 17:37:11 -0300

    fix: testing backend in vercel

--- server/index.js
+++ server/index.js
@@ -4,21 +4,18 @@ import mongoose from 'mongoose';
 import submitFormRoute from './api/submitForm.js';
 import dotenv from 'dotenv';
 
-// Cargar las variables de entorno
+// Cargar variables de entorno
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Conectado a MongoDB'))
-  .catch((err) => console.log('Error al conectar a MongoDB:', err));
+  .then(() => console.log('✅ Conectado a MongoDB'))
+  .catch((err) => console.log('❌ Error al conectar a MongoDB:', err));
 
 app.use('/api/submit-form', submitFormRoute);
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
+export default app;

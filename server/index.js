388c2172b25741a658661afb943b61172c003e70
SamuelSotomayor1 <andressotomayor477@gmail.com>
2025-04-01 17:39:15 -0300

    feat: comeback the normality

--- server/index.js
+++ server/index.js
@@ -4,18 +4,22 @@ import mongoose from 'mongoose';
 import submitFormRoute from './api/submitForm.js';
 import dotenv from 'dotenv';
 
-// Cargar variables de entorno
+// Cargar las variables de entorno
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('✅ Conectado a MongoDB'))
-  .catch((err) => console.log('❌ Error al conectar a MongoDB:', err));
+  .then(() => console.log('Conectado a MongoDB'))
+  .catch((err) => console.log('Error al conectar a MongoDB:', err));
 
 app.use('/api/submit-form', submitFormRoute);
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
+
